b4b7cf4612b9edd583d73e4f5e0182dd767f1aa0
Felipe Trevenzoli <felipe.trevenzoli@gmail.com>
2025-03-13 19:16:13 -0300

    feat: ajustes iniciais mids e rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 // api/src/routes/index.ts
 import express, { Request, Response } from 'express'
 import testRoutes from "./test"
+import userRouter from "./user"
 import { logging } from '../middlewares/logging'
 import { routeNotFound, errorHandler } from '../middlewares/errorHandlers'
 
@@ -9,6 +10,8 @@ const router = express.Router()
 router.use(logging)
 
 router.use('/test', testRoutes)
+
+router.use('/User', userRouter)
   
 router.get('/', (req: Request, res: Response) => {
     res.send('API Treis está funcionando! ')
