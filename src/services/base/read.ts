abb22d312d9eeee19d69072ce7b1798c2904a3a1
Felipe Trevenzoli <felipe.trevenzoli@gmail.com>
2025-04-15 22:01:52 -0300

    fix: read

--- src/services/base/read.ts
+++ src/services/base/read.ts
@@ -2,17 +2,35 @@ import { db } from '../../utils/db'
 import { BaseEntity, ReadData } from '../../types/entity'
 
 export const read = <T extends BaseEntity>(table: string) => {
+
+    const excludeTimestamps = <T extends BaseEntity>(entity: T): ReadData<T> => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { createdAt, updatedAt, deletedAt, ...rest } = entity
+        return rest
+    }
+
     const findAll = async (): Promise<ReadData<T>[]> => {
-        return db(table).whereNull('deletedAt').select('*');
-    };
+        return db(table)
+                .whereNull('deletedAt')
+                .select('*')
+                .then(rows => rows.map(excludeTimestamps))
+    }
 
     const findById = async (id: number): Promise<ReadData<T> | undefined> => {
-        return db(table).where({ id }).whereNull('deletedAt').first();
-    };
+        return db(table)
+                .where({ id })
+                .whereNull('deletedAt')
+                .first()
+                .then(result => result ? excludeTimestamps(result) : undefined)
+    }
 
     const findBy = async (filters: Partial<T>): Promise<ReadData<T>[] | undefined> => {
-        return db(table).where(filters).whereNull('deletedAt').select('*');
-    };
+        return db(table)
+                .where(filters)
+                .whereNull('deletedAt')
+                .select('*')
+                .then(rows => rows.map(excludeTimestamps))
+    }
 
-    return { findAll, findById, findBy };
-};
+    return { findAll, findById, findBy }
+}
