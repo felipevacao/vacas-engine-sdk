4aeed5133aac4eb11ed47a6f7c6aa63c02f2000c
Felipe Trevenzoli <felipe.trevenzoli@gmail.com>
2026-04-14 12:58:44 -0300

    fix: evitar agrupamento de contextos quando disable return errors

--- apps/treis/src/core/services/error.ts
+++ apps/treis/src/core/services/error.ts
@@ -1,14 +1,17 @@
 import { ErrorContext } from "@app-types/entity"
+import env from "@core/libs/env"
 
 export class ErrorService {
 
 	private errorContext: ErrorContext = {}
 
 	setErrorMetadata(metadata: Partial<ErrorContext>) {
+		if (!env.ENABLE_RETURN_ERRORS) return
 		this.errorContext = { ...this.errorContext, ...metadata }
 	}
 
 	setErrorMetadataDetails(detail: string) {
+		if (!env.ENABLE_RETURN_ERRORS) return
 		const detailsContext = this.errorContext.details || []
 		detailsContext.push(`${new Date()} ${detail}`)
 		this.setErrorMetadata({ details: detailsContext })
